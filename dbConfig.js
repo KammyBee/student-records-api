a3b417c27fbee5e1afd3c5e11b0857e637a7437d
KammyBee <kamdon.basinger@gmail.com>
2025-04-21 23:25:41 -0500

    final commit

--- dbConfig.js
+++ dbConfig.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); // Make sure this is called early
+ // Make sure this is called early
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect('mongodb://localhost/studentDB');
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
